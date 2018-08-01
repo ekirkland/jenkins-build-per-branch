5824f9b01c73e736f031ab95218cd6516060c842
Evan Kirkland <ekirkland@axonify.com>
2018-08-01 14:54:05 -0400

    Debugging

--- src/main/groovy/com/entagen/jenkins/GitApi.groovy
+++ src/main/groovy/com/entagen/jenkins/GitApi.groovy
@@ -26,6 +26,7 @@ class GitApi {
                 if(gitHubApiUrl && gitHubAccessToken) {
 
                     def response = ["curl", "-H", "Authorization: token ${gitHubAccessToken}", "${gitHubApiUrl}/commits/${commitSha}"].execute().text
+                    println response
                     def dateString = new JsonSlurper().parseText(response).commit.author.date;
                     def duration = new Date() - Date.parse("yyyy-MM-dd", dateString.substring(0, 10))
                     if(duration >= 14) {
